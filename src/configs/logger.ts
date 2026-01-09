1134de3a0c3359a8c7e176a319f3d4f7c996590d
minhtrifit <minhtri.fit@gmail.com>
2026-01-09 16:38:11 +0700

    feat: add logger

---
+++ src/configs/logger.ts
@@ -0,0 +1,32 @@
+import { createLogger, format, transports } from 'winston';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const { combine, timestamp, printf, colorize, errors } = format;
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+const logger = createLogger({
+  level: isProduction ? 'info' : 'debug',
+  format: combine(
+    errors({ stack: true }), // log cả stack trace
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
+  ),
+  transports: [
+    new transports.Console({
+      format: combine(colorize(), logFormat)
+    }),
+    new transports.File({
+      filename: 'logs/error.log',
+      level: 'error'
+    }),
+    new transports.File({
+      filename: 'logs/combined.log'
+    })
+  ]
+});
+
+export default logger;
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { Server } from 'socket.io';
 import i18nextMiddleware from 'i18next-http-middleware';
 import { errorHandler } from './middlewares/errorHandler';
 import { multerErrorHandler } from './middlewares/multer-error.middleware';
+import { morganMiddleware } from './configs/morgan';
 import { registerSocketEvents } from './sockets';
 import i18next from './configs/i18n';
 import { HTTP_STATUS } from './constants/http-status-code';
@@ -49,6 +50,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.resolve('uploads')));
 app.use(i18nextMiddleware.handle(i18next));
+app.use(morganMiddleware);
 app.use(express.json());
 
 // Routes
